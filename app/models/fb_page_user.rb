16588e3bf9c5c51e0580eb2b13438b19833b65d1
Richard Sumilang <me@richardsumilang.com>
2015-11-22 21:35:07 -0800

    Adding cleanup code for removing when a user is no longer a page admin

--- app/models/fb_page_user.rb
+++ app/models/fb_page_user.rb
@@ -6,6 +6,7 @@ class FbPageUser < ActiveRecord::Base
   validates :users_id, presence: true
 
   # This will sync the current pages the user manages
+  #
   # @param user
   def self.sync_user_pages(user)
     current_user_pages = FbPageUser.where(['users_id = ?', user.id])
@@ -16,14 +17,61 @@ class FbPageUser < ActiveRecord::Base
       indexed_user_pages[current_user_page.fb_page_id.to_f] = true
     end
 
-    # Loop through user accounts user is an admin for and add them to the table
-    user.facebook_accounts.each do |fb_user_account|
+    user_fb_accounts = user.facebook_accounts
+
+    # Add missing pages
+    self.add_user_to_pages(user, user_fb_accounts, indexed_user_pages)
+
+    # Remove pages no longer associated with
+    self.remove_user_from_pages(user, user_fb_accounts, indexed_user_pages)
+
+  end
+
+
+
+  private
+
+
+
+  # Loops through user's fb_accounts and adds fb page associations where missing
+  #
+  # @param user User account
+  #
+  # @param fb_accounts Fb Accounts
+  #
+  # @param current_pages A hash of the current pages accessible
+  def self.add_user_to_pages(user, fb_accounts, current_pages)
+    fb_accounts.each do |fb_user_account|
       create(
           users_id: user.id,
           fb_page_id: fb_user_account['id']
-      ) if !indexed_user_pages.has_key?(fb_user_account['id'].to_f)
+      ) if !current_pages.has_key?(fb_user_account['id'].to_f)
+    end
+  end
+
+
+  # Loops through user's fb_accounts and removes fb page associations where
+  # user is no longer an admin
+  #
+  # @param user User account
+  #
+  # @param fb_accounts Fb Accounts
+  #
+  # @param current_pages A hash of the current pages accessible
+  def self.remove_user_from_pages(user, fb_accounts, current_pages)
+    fb_pages = Hash.new
+
+    fb_accounts.each do |fb_user_account|
+      fb_pages[fb_user_account['id'].to_f] = true
+    end
+
+    current_pages.each do |fb_page_id, x|
+      if fb_pages.has_key?(fb_page_id)
+        FbPageUser.limit(1).delete_all(['users_id = ? AND fb_page_id = ?', user.id, fb_page_id])
+      end
     end
 
   end
+
 
 end
