fb2e043c741de90a38032494748eebb790f943f7
Richard Sumilang <me@richardsumilang.com>
2015-11-23 15:25:27 -0800

    Found an issue with sqlite "REPLACE INTO" generating a new id when replacing so I rewrote the function to only update if page info has changed and create new records for new pages found

--- app/models/fb_page.rb
+++ app/models/fb_page.rb
@@ -10,11 +10,39 @@ class FbPage < ActiveRecord::Base
   #
   # @param user
   def self.add_user_pages(user)
-    user.facebook_accounts.each do |page|
-      # @todo SQL Injection How to?
-      # @todo This could be written much much better... But eh, just an example.
-      FbPage.connection.insert "REPLACE INTO #{FbPage.table_name} (name, fb_page_id, category, created_at, updated_at) VALUES ('#{page['name']}', '#{page['id']}', '#{page['category']}', datetime(), datetime())"
+    # List of user FB pages
+    user_fb_pages = Hash.new
+    user.facebook_accounts.each do |user_fb_page|
+      user_fb_pages[user_fb_page['id'].to_s] = user_fb_page
     end
+
+    # Query the data and update it
+    fb_pages = where(fb_page_id: user_fb_pages.keys)
+    existing_ids = []
+    fb_pages.each do |fb_page|
+      existing_ids << fb_page['fb_page_id'].to_s
+      current_page_info = user_fb_pages[ fb_page['fb_page_id'].to_s ]
+      if fb_page['name'] != current_page_info['name'] and fb_page['category'] != current_page_info['category']
+        fb_page['name'] = current_page_info['name']
+        fb_page['category'] = current_page_info['category']
+        fb_page.save
+        logger.debug "Updating fb_pages[#{fb_page['id']}] with recent information"
+      else
+        logger.debug "Found fb_pages[#{fb_page['id']}] but nothing to do"
+      end
+    end
+
+    # Store new entries in db
+    new_pages = user_fb_pages.select { |k,v| !existing_ids.include?(k) }
+    new_pages.each do |fb_page|
+      create(
+        name: fb_page[1]['name'],
+        fb_page_id: fb_page[1]['id'],
+        category: fb_page[1]['category']
+      )
+      logger.debug "Inserting new fb_pages[#{fb_page[1]['id']}]"
+    end
+
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20151122221231) do
 
   create_table "fb_page_posts", force: :cascade do |t|
-    t.integer  "fb_page_id",       limit: 8
+    t.integer  "fb_page_id"
     t.integer  "fb_post_id",       limit: 8
     t.text     "message"
     t.integer  "share_count"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20151122221231) do
 
   create_table "fb_page_users", force: :cascade do |t|
     t.integer  "users_id"
-    t.integer  "fb_page_id", limit: 8
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.integer  "fb_page_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "fb_page_users", ["fb_page_id", "users_id"], name: "index_fb_page_users_on_fb_page_id_and_users_id", unique: true
